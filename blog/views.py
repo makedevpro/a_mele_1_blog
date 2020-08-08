4a277234dbedac95aaae53c820038c563757dbaa
makedevpro <makedevpro@gmail.com>
2020-08-08 18:46:30 +0300

    Similar posts show added in views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 
@@ -57,11 +58,29 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
         else:
             comment_form = CommentForm()
+
+    # Формирование списка похожих статей
+    # Метод QuerySet’а values_list() воз-
+    # вращает кортежи со значениями заданного поля. Мы указали flat=True,
+    # чтобы получить «плоский» список вида [1, 2, 3, ...]
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    # использует функцию агрегации Count для формирования вычисляемого
+    # поля same_tags, которое содержит определенное количество совпадаю-
+    # щих тегов;
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
+    # Django-taggit также включает менеджер similar_objects(), который мож-
+    # но использовать для поиска подобных объектов. Ознакомиться с полным опи-
+    # санием менеджеров django-taggit вы можете на странице
+    # https://django-taggit.readthedocs.io/en/latest/api.html.
     return render(request, 'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
