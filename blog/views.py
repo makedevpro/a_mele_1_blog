d229bbbe61f89f9c4a619d4f3364ed51da05d865
makedevpro <makedevpro@gmail.com>
2020-08-08 14:57:02 +0300

    CBV added, and keep function views too

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+
 
 from .models import Post
 
@@ -28,3 +30,11 @@ def post_detail(request, year, month, day, post):
                              publish__year=year, publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    # queryset вместо model, чтобы использовать свой менеджер published
+    queryset = Post.published.all()
+    context_object_name = 'posts'  # иначе будет object_list
+    paginate_by = 3  # ListView передает в контекст page_obj
+    template_name = 'blog/post/list_cbv.html'
