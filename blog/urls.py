1d948826e57c22b97b8214afb29d5a6cc228453f
makedevpro <makedevpro@gmail.com>
2020-08-08 17:05:13 +0300

    Comments for post's model is done

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # post views
     path('', views.PostListView.as_view(), name='post_list_cbv'),
     path('function_views/', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>',
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 def post_list(request):
@@ -31,7 +31,30 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # Список активных комментариев для данной статьи
+    comments = Comment.objects.filter(active=True)
+    new_comment = None
+
+    # Добавил по причине "local variable 'comment_form'
+    # referenced before assignment"
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        # Пользователь отправил комментарий.
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создаем комментарий, но пока не сохраняем в базе данных.
+            new_comment = comment_form.save(commit=False)
+            # Привязываем комментарий к текущей статье
+            new_comment.post = post
+            # Сохраняем комментарий в базе данных
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 
 class PostListView(ListView):
